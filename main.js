d898942d62d2c789ef5571db7940dddc1e88f696
vladislav-1999 <schoolschool2007@yandex.ru>
2025-07-31 20:14:40 +0300

    Added loading window

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ const headerNav__stack = document.querySelector(".header-nav__stack");
 const headerNnav__stackList = document.querySelector(".header-nav__stack-list");
 const headerNav__realProjects = document.querySelector(".header-nav__real-projects");
 const headerNav__realProjectsList = document.querySelector(".header-nav__real-projects-list");
-
+const preloader = document.querySelector(".preloader");
 
 headerNav__projects.addEventListener("mouseover", () => {
-    headerNav__projectsList.classList.remove('hidden')
+  headerNav__projectsList.classList.remove("hidden");
 });
 
 headerNav__projects.addEventListener("mouseout", () => {
@@ -17,7 +17,7 @@ headerNav__projects.addEventListener("mouseout", () => {
 });
 
 headerNav__stack.addEventListener("mouseover", () => {
-  headerNnav__stackList.classList.remove('hidden');
+  headerNnav__stackList.classList.remove("hidden");
 });
 
 headerNav__stack.addEventListener("mouseout", () => {
@@ -30,4 +30,9 @@ headerNav__realProjects.addEventListener("mouseover", () => {
 
 headerNav__realProjects.addEventListener("mouseout", () => {
   headerNav__realProjectsList.classList.add("hidden");
+});
+
+window.addEventListener("load", () => {
+  setTimeout(() => preloader.classList.add("zIndex0"), 500);
+  //   preloader.classList.add("zIndex0");
 });
